38ff28c2cf3d8ec1259991be5add6602eae61e45
iannicolasdev <nicolasian002@gmail.com>
2025-11-27 19:26:46 -0300

    exercise: Média Inteira

--- exercicios/neps_academy/entrada_saida/ex_1.cpp
+++ exercicios/neps_academy/entrada_saida/ex_1.cpp
@@ -1,3 +1,6 @@
+// Olá Neps Academy
+// https://neps.academy/br/exercise/1
+
 #include <iostream>
 
 using namespace std;
---
+++ exercicios/neps_academy/entrada_saida/ex_2.cpp
@@ -0,0 +1,16 @@
+// Média Inteira
+// https://neps.academy/br/exercise/136
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int A, B;
+
+    cin >> A >> B;
+    
+    cout << (A + B) / 2;
+
+    return 0;
+}
