832cb1104b7d5d55d7f39467fb5fb3e4cafaf39d
iannicolasdev <nicolasian002@gmail.com>
2025-11-28 14:21:15 -0300

    execise: Comando ESCREVA

---
+++ exercicios/neps_academy/entrada_saida/ex_4.cpp
@@ -0,0 +1,13 @@
+// Exercício: Comando ESCREVA
+// Dificuldade: Super Fácil
+// Link: https://neps.academy/br/exercise/75
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Curso de Portugol do Sucesso";
+
+    return 0;
+}
