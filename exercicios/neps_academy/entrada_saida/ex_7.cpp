7a7c5acd20a125404903d0fff9cb4657ced8be48
iannicolasdev <nicolasian002@gmail.com>
2025-11-28 21:36:44 -0300

    Lanche na padaria

---
+++ exercicios/neps_academy/entrada_saida/ex_7.cpp
@@ -0,0 +1,18 @@
+// Exercício: Lanche na padaria
+// Dificuldade: Fácil
+// Link: https://neps.academy/br/exercise/675
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int valor;
+
+    cin >> valor;
+
+    if (valor <= 5) cout << "SIM";
+    else cout << "NAO";
+
+    return 0;
+}
