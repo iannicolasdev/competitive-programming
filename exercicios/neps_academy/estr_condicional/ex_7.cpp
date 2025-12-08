61af09d1499843c35659056ce64d0d2c8ae9c6dc
iannicolasdev <nicolasian002@gmail.com>
2025-12-08 20:36:23 -0300

    exercise: Lâmpadas

---
+++ exercicios/neps_academy/estr_condicional/ex_7.cpp
@@ -0,0 +1,31 @@
+// Exercício: Lâmpadas
+// Dificuldade: Médio
+// Link: https://neps.academy/br/exercise/52
+
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+    
+    int N;
+    bool l1 = false, l2 = false;
+
+    cin >> N;
+    int quantidade[N];
+
+    for (int i = 0; i < N; i++) cin >> quantidade[i];
+    
+    for (int i = 0; i < N; i++) {
+        if (quantidade[i] == 1) l1 = !l1;
+        if (quantidade[i] == 2) {
+            l1 = !l1;
+            l2 = !l2;
+        }
+    }
+
+    cout << l1 << "\n" << l2;
+    
+    return 0;
+}
