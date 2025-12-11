58b57b1b01c671ff30edda983aae9bb1675974e7
iannicolasdev <nicolasian002@gmail.com>
2025-12-11 15:34:06 -0300

    exercise: Repetir X Vezes

---
+++ exercicios/neps_academy/estr_repeticao/157.cpp
@@ -0,0 +1,17 @@
+// Exercício: Repetir X Vezes 
+// Dificuldade: Super Fácil
+// Link: https://neps.academy/br/exercise/157
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int X;
+
+    cin >> X;
+
+    for(int i = 0; i < X; i++) cout << "NepsAcademy eh Sucesso\n";
+
+    return 0;
+}
