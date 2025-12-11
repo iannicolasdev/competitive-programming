1cedf3834d8cb00ddc6ac1e50308fa6b10372290
iannicolasdev <nicolasian002@gmail.com>
2025-12-11 14:32:50 -0300

    exercise: Tabuada

---
+++ exercicios/neps_academy/estr_repeticao/154.cpp
@@ -0,0 +1,17 @@
+// Exercício: Tabuada
+// Dificuldade: Fácil
+// Link: https://neps.academy/br/exercise/154
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int X;
+
+    cin >> X;
+
+    for (int i = 1; i <= 10; i++) cout << X << " * " << i << " = " << X * i << "\n";
+    
+    return 0;
+}
