7115c8d94449fbfc55557dac5f4642fe5ef97cc4
iannicolasdev <nicolasian002@gmail.com>
2025-12-14 09:42:40 -0300

    execise: Quadrante

---
+++ exercicios/neps_academy/estr_condicional/146.cpp
@@ -0,0 +1,21 @@
+// Exercício: Quadrante
+// Dificuldade: Fácil
+// Link: https://neps.academy/br/exercise/146
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int X, Y;
+
+    cin >> X >> Y;
+
+    if (X == 0 || Y == 0) cout << "eixos";
+    else if (X > 0 && Y > 0) cout << "Q1";
+    else if (X < 0 && Y > 0) cout << "Q2";
+    else if (X < 0 && Y < 0) cout << "Q3";
+    else if (X > 0 && Y < 0) cout << "Q4";
+
+    return 0;
+}
