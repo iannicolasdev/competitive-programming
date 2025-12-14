626207bc21a2636cb4efa2848b2c342d72b5723b
iannicolasdev <nicolasian002@gmail.com>
2025-12-14 08:58:00 -0300

    Execise: Empilhamento Tetral

---
+++ exercicios/neps_academy/entrada_saida/2519.cpp
@@ -0,0 +1,18 @@
+// Exercício: Empilhamento Tetral
+// Dificuldade: Super Fácil
+// Link: https://neps.academy/br/exercise/2519
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string A = ".-----.\n";
+    string B = "|     |\n|     |\n";
+    string C = "|-----|\n";
+
+    cout << A << B << C << B << C << B << C << B << A;
+
+    return 0;
+}
+
