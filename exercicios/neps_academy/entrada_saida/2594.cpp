53345929919d4c38ed85d6c9587c2c6e350f5031
iannicolasdev <nicolasian002@gmail.com>
2025-12-15 20:31:23 -0300

    exercise: Helicóptero

---
+++ exercicios/neps_academy/entrada_saida/2594.cpp
@@ -0,0 +1,19 @@
+// Exercício: Helicóptero
+// Dificuldade: Super Fácil
+// Link: https://neps.academy/br/exercise/2594
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "******=================== \n";
+    cout << "**********____||___ \n";
+    cout << "\\*/******/*******[]\\ \n";
+    cout << "*X=======***********\\__ \n";
+    cout << "/*\\******\\____________| \n";
+    cout << "************||**|| \n";
+    cout << "*********\\-----------/";
+
+    return 0;
+}
